6e3a694d3453b28fae6dc50227ce9b67bad06be7
Vladyslav Yelnikov <31829943+vlad-yelnikov@users.noreply.github.com>
2020-07-22 20:14:23 +0300

    Card module (#2)
    * add crud
    
    * add swagger packages
    
    * add some changes

---
+++ src/card/card.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Card } from 'src/card/card.entity'
+import { CardController } from './card.controller';
+import { CardService } from './card.service';
+@Module({
+  imports: [TypeOrmModule.forFeature([Card])],
+  controllers: [CardController],
+  providers: [CardService]
+})
+export class CardModule {}
